ffb6b1124adb4e9322451d99d9709754dc45a29f
Alexander Dymo <adymo@kdevelop.org>
2012-03-11 09:17:33 +0200

    Better upload command: it doesn't require any arguments anymore, just uploads what has to be uploaded starting with current dir (run smug status to see what will be uploaded) Also it can create albums now.

--- lib/smugctl/upload.rb
+++ lib/smugctl/upload.rb
@@ -5,67 +5,73 @@ module Smug
 
 class UploadCommand < Command
 
+    # current upload algorithm:
+    # 1. get the list not uploaded stuff starting from current dir and below
+    # 2. chdir to root (for simplicity)
+    # 3. for each album
+    #   3.1. create it if it doesn't exist
+    #   3.2. upload files
     def exec
-        album_id = ARGV[0]
-        unless album_id
-            $stderr.puts "missing album id"
-            exit(1)
-        end
-        album_key = ARGV[1]
-        unless album_key
-            $stderr.puts "missing album key"
-            exit(1)
-        end
-        files_to_upload = ARGV[2, ARGV.length]
-        if files_to_upload.empty?
-            $stderr.puts "specify files to upload"
-            exit(1)
-        end
-
         authenticate
 
-        existing_files = get("smugmug.images.get", {
-            :AlbumID => album_id,
-            :AlbumKey => album_key,
-            :Heavy => true
-        })["Album"]["Images"].map {|p| p["FileName"].downcase }
+        status = StatusCommand.new.current_dir_status
+        Dir::chdir(Config::root_dir)
 
-
-        num_files = files_to_upload.length
-        files_to_upload.each_with_index do |filename, i|
-            if existing_files.include? filename.downcase
-                puts "Skipping #{filename}: already exists"
+        status.each_with_index do |album_status, i|
+            if album_status[:status] == :not_uploaded
+                # create album
+                result = smugmug_albums_create(
+                    "Title" => album_status[:album]
+                )
+                raise "Cannot create album" unless result["stat"] == "ok"
+                # refresh cache
+                FetchCommand.new.exec
+            elsif album_status[:status] == :not_downloaded
                 next
             end
 
-            begin
-                SystemTimer.timeout_after(1800) do
-                    puts "Uploading #{filename} (#{Time.now.to_s}) (#{i}/#{num_files})"
+            album = albums.find { |a| a["Title"] == album_status[:album] }
 
-                    data = File.open(filename, "rb") { |f| f.read }
+            files_to_upload = album_status[:images].find_all { |i| i[:status] == :not_uploaded }.map { |i| "#{album["Title"]}/#{i[:image]}" }
 
-                    req = {}
-                    req['Content-Length'] = File.size(filename).to_s
-                    req['Content-MD5'] = MD5.hexdigest(data)
-                    req['X-Smug-AlbumID'] = album_id
-                    req['X-Smug-Version'] = '1.3.0'
-                    req['X-Smug-FileName'] = filename
-                    req['X-Smug-ResponseType'] = "JSON"
+            num_files = files_to_upload.length
+            files_to_upload.each_with_index do |filename, i|
+                begin
+                    SystemTimer.timeout_after(1800) do
+                        puts "Uploading #{filename} (#{Time.now.to_s}) (#{i}/#{num_files})"
 
-                    results = put("http://upload.smugmug.com/#{filename}", data, req)
-                    puts " => #{results["stat"]} (#{Time.now.to_s})"
+                        data = File.open(filename, "rb") { |f| f.read }
+
+                        req = {}
+                        req['Content-Length'] = File.size(filename).to_s
+                        req['Content-MD5'] = MD5.hexdigest(data)
+                        req['X-Smug-AlbumID'] = album["id"].to_s
+                        req['X-Smug-Version'] = '1.3.0'
+                        req['X-Smug-FileName'] = filename
+                        req['X-Smug-ResponseType'] = "JSON"
+
+                        results = put("http://upload.smugmug.com/#{filename}", data, req)
+                        puts " => #{results["stat"]} (#{Time.now.to_s})"
+                    end
+                rescue Timeout::Error
+                    puts " => timed out"
+
+                    # TODO: delete image from server
+                rescue Exception => e
+                    puts " => error #{e.message}"
+                    puts e.backtrace.join("\n")
+
+                    # TODO: delete image from server
                 end
-            rescue Timeout::Error
-                puts " => timed out"
-
-                # TODO: delete image from server
-            rescue Exception => e
-                puts " => error #{e.message}"
-                puts e.backtrace.join("\n")
-
-                # TODO: delete image from server
             end
         end
+    end
+
+private
+
+    def albums
+        # TODO: upload doesn't work without cache
+        @albums ||= JSON::parse Config::config_file("cache", "r").read
     end
 
 end
