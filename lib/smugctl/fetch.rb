3adc3e86b8b5d3bb23be8a8e61d317a1b36e3383
Alexander Dymo <adymo@kdevelop.org>
2012-03-16 08:50:39 +0200

    Don't refetch album images if album hasn't changed since we last cached it

--- lib/smugctl/fetch.rb
+++ lib/smugctl/fetch.rb
@@ -7,10 +7,21 @@ class FetchCommand < Command
     def exec
         authenticate
 
+        cached_albums = JSON::parse Config::config_file("cache", "r").read
+
         status_message "Refreshing albums cache"
-        albums = smugmug_albums_get["Albums"]
+        albums = smugmug_albums_get(:Heavy => true)["Albums"]
         status_message "."
+
         albums.each do |album|
+            # getting the list of images from album is a lengthy operation
+            # so skip it if album hasn't changed
+            cached_album = cached_albums.find { |a| a["id"] == album["id"] }
+            if cached_album and album["LastUpdated"] == album["LastUpdated"]
+                status_message "."
+                next
+            end
+
             images = smugmug_images_get(:AlbumID => album["id"], :AlbumKey => album["Key"], :Heavy => true)["Album"]["Images"]
             album["Images"] = images
             status_message "."
